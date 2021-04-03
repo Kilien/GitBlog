16824fbf68279cee2e6c7021abbe547beebeee63
KiLien <kilienazure@gmail.com>
2021-04-04 01:33:51 +0800

    update 风铃样式

--- static/js/custom.js
+++ static/js/custom.js
@@ -6,13 +6,13 @@ $(function () {
 		  "https://cdn.jsdelivr.net/gh/KiLien/Pics/wallhaven/wallhaven-yjk6ml.jpg"
 	], { duration: 60000, fade: 1500 });
 });
-/* 拉姆蕾姆回到顶部或底部按钮 */
+/* 风铃🎐回到顶部或底部按钮 */
 $(function() {
-	$("#lamu img").eq(0).click(function() {
+	$("#right-wind-chimes .wind-chimes-img").eq(0).click(function() {
 		$("html,body").animate({scrollTop:$(document).height()},800);
 		return false;
 	});
-	$("#leimu img").eq(0).click(function() {
+	$("#left-wind-chimes .wind-chimes-img").eq(0).click(function() {
 		$("html,body").animate({scrollTop:0},800);
 		return false;
 	});
