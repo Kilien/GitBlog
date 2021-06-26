f640b7d050963aa0ed313e1aee98f81ba0b157a7
KiLien <kilienazure@gmail.com>
2021-06-26 17:01:38 +0800

    add comment

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,11 +1,11 @@
-/* 轮播背景图片 */
-$(function () {
-	$.backstretch([
-          "https://cdn.jsdelivr.net/gh/KiLien/Pics/wallhaven/wallhaven-vgwvy5.jpg",
-		  "https://cdn.jsdelivr.net/gh/KiLien/Pics/wallhaven/wallhaven-odkykm.jpg",
-		  "https://cdn.jsdelivr.net/gh/KiLien/Pics/wallhaven/wallhaven-yjk6ml.jpg"
-	], { duration: 60000, fade: 1500 });
-});
+// /* 轮播背景图片 */
+// $(function () {
+// 	$.backstretch([
+//           "https://cdn.jsdelivr.net/gh/KiLien/Pics/wallhaven/wallhaven-vgwvy5.jpg",
+// 		  "https://cdn.jsdelivr.net/gh/KiLien/Pics/wallhaven/wallhaven-odkykm.jpg",
+// 		  "https://cdn.jsdelivr.net/gh/KiLien/Pics/wallhaven/wallhaven-yjk6ml.jpg"
+// 	], { duration: 60000, fade: 1500 });
+// });
 /* 风铃🎐回到顶部或底部按钮 */
 $(function() {
 	$("#right-wind-chimes .wind-chimes-img").eq(0).click(function() {
